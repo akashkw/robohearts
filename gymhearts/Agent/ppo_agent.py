93a2f8ea6ab45e1db61839a6566a183cdd529dba
Lucaskabela <lucaskabela@utexas.edu>
2019-12-05 10:22:40 -0600

    Add code for ppo locally

---
+++ gymhearts/Agent/ppo_agent.py
@@ -0,0 +1,142 @@
+import random
+import numpy as np
+from datetime import datetime
+import torch
+import copy
+
+from .agent_utils import *
+from .hand_approx import *
+from .reinforce import *
+
+class PPO_Agent:
+    def __init__(self, name, params=dict()):
+        # Game Params
+        self.name = name
+        self.print_info = params.get('print_info', False)
+
+        # Agent Params
+        self.GAMMA = params.get('gamma', .95)
+        self.ALPHA = params.get('alpha', 3e-4)
+
+        # NN params
+        pi_path = params.get('pi_saved', False)
+        baseline_path = params.get('base_saved', False)
+        self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        self.pi = PiApproximationWithNN(52, 52, self.ALPHA, pi_path)
+        self.baseline = VApproximationWithNN(52, self.ALPHA, baseline_path)
+
+        # optimizer params
+        self.optim = torch.optim.Adam(self.nn.parameters(), lr=self.ALPHA)
+
+        # fn approx items:
+        self.FT_LIST = []
+        self.p_idx = []
+        self.scores = [0]*4
+        self.played_cards=[]
+        self.won_cards=[]
+        for i in range(4):
+            self.won_cards.append([])
+
+        if params.get('in_hand', True):
+            self.FT_LIST.append('in_hand')
+        if params.get('in_play', False):
+            self.FT_LIST.append('in_play')
+        if params.get('played_cards', False):
+            self.FT_LIST.append('played_cards')
+        if params.get('cards_won', False):    
+            self.FT_LIST.append('cards_won')
+        if params.get('scores', False):    
+            self.FT_LIST.append('scores')
+
+    def Do_Action(self, observation):
+
+        if observation['event_name'] == 'GameStart':
+            players = observation['data']['players']
+            for player in players:
+                self.p_idx.append(player['playerName'])
+
+        elif observation['event_name'] == 'PassCards':
+            if self.print_info:
+                print(handle_event(observation))
+            passCards = random.sample(observation['data']['hand'],3)
+            
+            if self.print_info:
+                print(self.name, 'is passing ::', " ".join([pretty_card(card) for card in passCards]))
+                
+            return {
+                    "event_name" : "PassCards_Action",
+                    "data" : {
+                        'playerName': self.name,
+                        'action': {'passCards': passCards}
+                    }
+                }
+
+        elif observation['event_name'] == 'PlayTrick':
+            if self.print_info:
+                print(handle_event(observation))
+
+            hand = observation['data']['hand']
+            if '2c' in hand:
+                choose_card = '2c'
+            else:
+                action = self.action_select(observation)
+                choose_card = deck_reference()[action]
+                if self.print_info:
+                    print(self.name, 'chose card ::', pretty_card(choose_card))
+
+            return {
+                    "event_name" : "PlayTrick_Action",
+                    "data" : {
+                        'playerName': self.name,
+                        'action': {'card': choose_card}
+                    }
+                }
+
+        elif observation['event_name'] == 'NewRound':
+            players = observation['data']['players']
+            for i, player in enumerate(players):
+                self.scores[i] = player['score']
+
+        elif observation['event_name'] == 'ShowTrickEnd':
+            winner = self.p_idx.index(observation['data']['trickWinner'])
+            for card in observation['data']['cards']:
+                self.won_cards[winner].append(card)
+                self.played_cards.append(card)
+
+        elif observation['event_name'] == 'RoundEnd':
+            self.played_cards = []
+            self.won_cards = []
+            for i in range(4):
+                self.won_cards.append([])
+
+    def update_weights(self, episode_info):
+        G = 0
+        for i, result in enumerate(reversed(episode_info)):
+            t = len(episode_info) - i - 1
+            state, filter, action, reward = result
+            G = G*self.GAMMA + reward
+            delta = G - self.baseline(state)
+            self.baseline.update(state, G)
+            self.pi.update(state, action, (self.GAMMA**t), delta, filter)
+
+    # Select an action using epsilon-greedy action selection
+    def action_select(self, observation):
+        ft =  torch.tensor(features_state(observation)).to(self.device)
+        filter = self.get_filter(observation)
+        return self.pi(features, filter).detach().item()
+
+    def features_state(self, observation):
+        return get_features(observation, feature_list=self.FT_LIST, 
+            played_cards=self.played_cards, won_cards=self.won_cards, scores=self.scores)
+
+    def get_filter(self, observation):
+        val_moves = filter_valid_moves(observation)
+        deck_ref = deck_reference()
+        indices_valid = [deck_ref.index(card) for card in val_moves]
+        filter = torch.zeros(52)
+        for index in indices_valid:
+            filter[index] = 1
+        return filter
+
+    def get_state_info(self, observation):
+        return self.features_state(observation), self.get_filter(observation)
---
+++ gymhearts/Agent/reinforce.py
@@ -0,0 +1,128 @@
+from typing import Iterable
+import numpy as np
+import torch
+from torch.distributions import Categorical
+import torch.nn.functional as F
+
+class PiApproximationWithNN():
+    def __init__(self,
+                 state_dims,
+                 num_actions,
+                 alpha,
+                 saved=None):
+        """
+        state_dims: the number of dimensions of state space
+        action_dims: the number of possible actions
+        alpha: learning rate
+        """
+        # TODO: implement here
+        self.nn = MLPClassifier(n_input_features=state_dims, n_output_features=num_actions, softmax=False)
+        if saved is not None:
+            self.nn.load_state_dict(load(path.join(path.dirname(path.abspath(__file__)), 'pi.th'), map_location='cpu'))
+        self.optim = torch.optim.Adam(self.nn.parameters(), lr=alpha)
+
+    def __call__(self,s, valid_filter) -> int:
+        # WRITE VALID_FILTER, MASK INVALID CARDS FROM SAMPLE
+        # Sample an action according to the policy
+        logits = self.nn(torch.FloatTensor(s))
+        logit_fiter = logits * valid_filter
+        return sample_categorical(logits=logit_fiter)
+       
+
+    def update(self, s, a, gamma_t, delta, valid_filter):
+        """
+        s: state S_t
+        a: action A_t
+        gamma_t: gamma^t
+        delta: G-v(S_t,w)
+        """
+        self.optim.zero_grad()
+        p = Categorical(self.nn(torch.FloatTensor(s)) * valid_filter)
+        l = p.log_prob(torch.tensor(a))
+        (-l*delta*gamma_t).backward()
+        self.optim.step()
+
+    def save_pi_model():
+        from torch import save
+        from os import path
+        return save(self.nn.state_dict(), path.join(path.dirname(path.abspath(__file__)), 'pi.th'))
+
+def sample_categorical(out):
+    return Categorical(probs=out).sample().item()
+
+class Baseline(object):
+    """
+    The dumbest baseline; a constant for every state
+    """
+    def __init__(self,b):
+        self.b = b
+
+    def __call__(self,s) -> float:
+        return self.b
+
+    def update(self,s,G):
+        pass
+
+class MLPClassifier(torch.nn.Module):
+    def __init__(self, n_input_features=52, n_output_features=1, n_hidden=256, softmax=False):
+        super().__init__()
+
+        """
+        Your code here
+        """
+        if softmax:       
+            self.network = torch.nn.Sequential(
+                torch.nn.Linear(n_input_features, n_hidden),
+                torch.nn.ReLU(),
+                torch.nn.Linear(n_hidden, n_hidden),
+                torch.nn.ReLU(),
+                torch.nn.Linear(n_hidden, n_output_features),
+                torch.nn.Softmax(dim=0),
+            )
+        else:
+            self.network = torch.nn.Sequential(
+                torch.nn.Linear(n_input_features, n_hidden),
+                torch.nn.ReLU(),
+                torch.nn.Linear(n_hidden, n_hidden),
+                torch.nn.ReLU(),
+                torch.nn.Linear(n_hidden, n_output_features),
+            )
+
+    def forward(self, x):
+        """
+        Your code here
+
+        @x: torch.Tensor((B, n_input_features))
+        @return: torch.Tensor((B, 1))
+        """
+        out = self.network(x)
+        return out
+
+class VApproximationWithNN(Baseline):
+    def __init__(self,
+                 state_dims,
+                 alpha,
+                 saved=None):
+        """
+        state_dims: the number of dimensions of state space
+        alpha: learning rate
+        """
+        self.nn = MLPClassifier(n_input_features=state_dims)
+        self.optimizer = torch.optim.Adam(self.nn.parameters(), lr=alpha)
+        if saved is not None:
+            self.nn.load_state_dict(load(path.join(path.dirname(path.abspath(__file__)), 'baseline.th'), map_location='cpu'))
+
+    def __call__(self,s):
+        return self.nn(torch.FloatTensor(s)).detach().item()
+
+    def update(self, s, G):
+        val = self.nn(torch.FloatTensor(s))
+        returns = torch.tensor([G])
+        self.optimizer.zero_grad()
+        (.5 * F.mse_loss(val, returns)).backward()
+        self.optimizer.step()
+
+    def save_pi_model():
+        from torch import save
+        from os import path
+        return save(self.nn.state_dict(), path.join(path.dirname(path.abspath(__file__)), 'baseline.th'))
